b63c65712f43a506a731c07cb77272bcf121418c
ereanha <ereanha@gmail.com>
2013-05-08 23:12:49 +0100

    hello world

---
+++ python/bottle/helloWorld.py
@@ -0,0 +1,14 @@
+from bottle import route, run, template
+
+@route('/')
+@route('/hello')
+def hello():
+    return "Hello World!"
+
+
+@route('/hello/<name>')
+def greet(name='Stranger'):
+    return template('Hello {{name}}, how are you?', name=name)
+
+
+run(host='localhost', port=8080, debug=True)
